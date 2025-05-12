e8e82b116ec7b3282179d3e1c52882347499a4c6
samuelsoaress <samuelsoares177778@gmail.com>
2025-05-12 15:30:37 -0300

    feat: conteiner para deploy

---
+++ script-deploy.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+chmod 600 /home/ubuntu/.env
+
+if [ -f techblood-agency.tar ]; then
+  rm techblood-agency.tar
+fi
+echo "Carregando imagem no Docker..."
+gzip -d techblood-agency.tar.gz
+
+if [ $? -ne 0 ]; then
+  echo "Erro ao descompactar o arquivo ajr-solucoes.tar.gz"
+  exit 1
+fi
+echo "Descompactado com sucesso"
+echo "Carregando imagem no Docker..."
+docker load -i ajr-solucoes.tar
+
+mv docker-compose-prod.yaml docker-compose.yaml
+
+container_ids=$(docker ps -q)
+
+if [ -z "$container_ids" ]; then
+  echo "Nao ha containers em execucao"
+else
+  for container_id in $container_ids; do
+    echo "Parando container: $container_id"
+    docker stop $container_id
+  done
+  echo "Todos os containers em execucao foram parados. "
+fi 
+
+echo "Subindo containers"
+docker compose up -d
